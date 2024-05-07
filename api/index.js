4a1d7eb39a0cfd550721e49166f73ee6738d23cd
Ankit Singh <kankit052@gmail.com>
2024-05-07 15:50:26 +0530

    client removeed3

--- api/index.js
+++ api/index.js
@@ -22,25 +22,7 @@ app.use('/items' , itemRoute);
 app.use('/user', userRoute);
 
 
-app.get('/', (req, res,next) => {
-    try {
-        res.json({ data: 'hi' });
-  
-   let result = null
-      // If the data is not found, throw a custom error
-      if (!result) {
-        throw new Error(404, 'Data not found');
-      }
-  
-      // Send the response with the data
-      res.json({ data: result });
-  
-    } catch (error) {
-      // Pass the error to the error handling middleware
-      next(error);
-  
-    }
-  });
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // Start the server
 app.listen(PORT, () => {

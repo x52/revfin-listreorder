55d90b6bd01c5c7579d01079ee7a5efde63618cb
Ankit Singh <kankit052@gmail.com>
2024-05-07 15:42:54 +0530

    client removeed2

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler');
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(express.json());
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
